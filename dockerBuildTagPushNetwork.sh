8ca01aa8cb08a9a9732c35488df09e3fd5185cd8
Angelo Romito <angelo.romito@de.markant.com>
2021-07-01 21:34:05 +0200

    Updates docker file

--- dockerBuildTagPushNetwork.sh
+++ dockerBuildTagPushNetwork.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker login -u trundicho
-docker build -t time-clock-stamper-ws:1.2 .
-docker tag time-clock-stamper-ws:1.2 trundicho/time-clock-stamper-ws:1.2
-docker push trundicho/time-clock-stamper-ws:1.2
-docker network create mynetwork
-docker network connect mynetwork time-clock-stamper-ws
+sudo docker login -u trundicho
+sudo docker build -t time-clock-stamper-ws:1.2 .
+sudo docker tag time-clock-stamper-ws:1.2 trundicho/time-clock-stamper-ws:1.2
+sudo docker push trundicho/time-clock-stamper-ws:1.2
+sudo docker network create mynetwork
+sudo docker network connect mynetwork time-clock-stamper-ws
