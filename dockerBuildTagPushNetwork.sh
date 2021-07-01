f3ee8f37c03224b3d0d70930f22c2fe50d41cd39
Angelo Romito <angelo.romito@de.markant.com>
2021-07-01 23:07:59 +0200

    Updates docker script

--- dockerBuildTagPushNetwork.sh
+++ dockerBuildTagPushNetwork.sh
@@ -4,4 +4,6 @@ sudo docker build -t time-clock-stamper-ws:1.2 .
 sudo docker tag time-clock-stamper-ws:1.2 trundicho/time-clock-stamper-ws:1.2
 sudo docker push trundicho/time-clock-stamper-ws:1.2
 sudo docker network create mynetwork
-sudo docker network connect mynetwork time-clock-stamper-ws
+sudo docker container stop time-clock-stamper-ws
+sudo docker container rm time-clock-stamper-ws
+sudo docker run --network mynetwork --name time-clock-stamper-ws --mount type=bind,src=/Users/angeloromito/Desktop/Stempeluhr/,dst=/workspace/ -p 8081:8080 time-clock-stamper-ws:1.2
