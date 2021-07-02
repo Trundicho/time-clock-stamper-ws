e69f3ea38bfedfee640fe9596c916d51dec74e52
Angelo Romito <angelo.romito@de.markant.com>
2021-07-02 18:02:59 +0200

    Updates docker script

--- dockerize.sh
+++ dockerize.sh
@@ -3,6 +3,8 @@ image=time-clock-stamper-ws
 version=1.2
 network=mynetwork
 hostport=8081
+hostShare=/Users/angeloromito/Desktop/Stempeluhr/
+containerShare=/workspace/
 sudo docker login -u trundicho
 sudo docker build -t $image:$version .
 sudo docker tag $image:$version trundicho/$image:$version
@@ -10,4 +12,4 @@ sudo docker push trundicho/$image:$version
 sudo docker network create $network
 sudo docker container stop $image
 sudo docker container rm $image
-sudo docker run --network $network --name $image -p $hostport:8080 $image:$version
+sudo docker run --network $network --name $image --mount type=bind,src=$hostShare,dst=$containerShare -p $hostport:8080 $image:$version
