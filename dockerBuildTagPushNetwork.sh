a0b5cf053fdf86c40b76cb798ac262bc54cd9765
Angelo Romito <angelo.romito@de.markant.com>
2021-07-02 17:54:36 +0200

    Updates docker script

--- dockerBuildTagPushNetwork.sh
+++ dockerBuildTagPushNetwork.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+image=time-clock-stamper-ws
+version=1.2
+network=mynetwork
+hostport=8081
 sudo docker login -u trundicho
-sudo docker build -t time-clock-stamper-ws:1.2 .
-sudo docker tag time-clock-stamper-ws:1.2 trundicho/time-clock-stamper-ws:1.2
-sudo docker push trundicho/time-clock-stamper-ws:1.2
-sudo docker network create mynetwork
-sudo docker container stop time-clock-stamper-ws
-sudo docker container rm time-clock-stamper-ws
-sudo docker run --network mynetwork --name time-clock-stamper-ws --mount type=bind,src=/Users/angeloromito/Desktop/Stempeluhr/,dst=/workspace/ -p 8081:8080 time-clock-stamper-ws:1.2
+sudo docker build -t $image:$version .
+sudo docker tag $image:$version trundicho/$image:$version
+sudo docker push trundicho/$image:$version
+sudo docker network create $network
+sudo docker container stop $image
+sudo docker container rm $image
+sudo docker run --network $network --name $image -p $hostport:8080 $image:$version
